763df04b8e4db347d4759fa54bfc265d040c1e76
senator.temp@gmail.com <timurusmanov@MacBook-Air-Timur.local>
2015-08-15 16:42:00 +0600

    Test

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -6,20 +6,26 @@
 //  Copyright (c) 2015 Timur Usmanov. All rights reserved.
 //
 
+// import UIKit
+
+// class ViewController: UIViewController {
+
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view, typically from a nib.
+//    }
+
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+ //   }
+
+
+//}
+
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    @IBOutlet weak var Display: UILabel!
 }
-
